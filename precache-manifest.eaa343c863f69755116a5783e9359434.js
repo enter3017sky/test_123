2bc195f775b941b7e76d7a33bb47ee15cbf397ef
黄信越 <clark.huang@timecitytech.com>
2021-01-19 18:04:46 +0800

    Updates

---
+++ precache-manifest.eaa343c863f69755116a5783e9359434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1894315da8e710b3b5d",
+    "url": "/static/css/main.3f5ec75e.chunk.css"
+  },
+  {
+    "revision": "e1894315da8e710b3b5d",
+    "url": "/static/js/main.0a24d549.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "629175a71178f4874a35",
+    "url": "/static/js/2.c9ae4213.chunk.js"
+  },
+  {
+    "revision": "be5278c5dc4e1d913ad075c6d31c5680",
+    "url": "/static/media/product-loading.be5278c5.gif"
+  },
+  {
+    "revision": "26369b187dd31838b9be4ae24efdcdfc",
+    "url": "/index.html"
+  }
+];
